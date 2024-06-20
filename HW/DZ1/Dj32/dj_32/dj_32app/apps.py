b89334c54c563ff266591a403fdfd982a0e05dc6
T1M0W4 <150163671+T1M0W4@users.noreply.github.com>
2024-06-20 16:56:54 +0500

    HW1

---
+++ HW/DZ1/Dj32/dj_32/dj_32app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Dj32AppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dj_32app'
--- HW/DZ1/Dj32/djvenv/Scripts/activate.bat
+++ HW/DZ1/Dj32/djvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\iTSTEP(studies)\Github\django\HW\DZ1\Proj_HW\myvenv
+set VIRTUAL_ENV=D:\iTSTEP(studies)\Github\django\HW\DZ1\Dj32\djvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(myvenv) %PROMPT%
+set PROMPT=(djvenv) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(myvenv) 
+set VIRTUAL_ENV_PROMPT=(djvenv) 
 
 :END
 if defined _OLD_CODEPAGE (
